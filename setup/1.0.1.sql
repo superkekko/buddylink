0c32a6850bd90ebe08fa6355830dfc164f21ba68
Francesco <mrfrancesco@gmail.com>
2024-01-17 17:24:23 +0100

    fix setup

--- setup/1.0.1.sql
+++ setup/1.0.1.sql
@@ -1,4 +1,4 @@
 ALTER TABLE link_list RENAME COLUMN status TO group_id;
 ALTER TABLE link_list ADD share INTEGER;
-update user set group_id=user_id where group_id is null;
+update user set group_id=user_id where group_id is null or group_id='';
 update link_list set group_id=(select u.group_id from user u where u.user_id=user_ins) where group_id is null;
